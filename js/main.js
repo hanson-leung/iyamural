8871b7767142e26e044649505ab1e4783ccb35b1
Hanson Leung <hanson@hleung.com>
2021-10-05 16:31:23 -0700

    create homepage

---
+++ js/main.js
@@ -0,0 +1,77 @@
+// toggle dropdown on click
+$(".card").on("click", function () {
+  $(this).find(".dropdown").toggleClass("clicked");
+});
+
+// get current year
+// document.getElementById("year").innerHTML = new Date().getFullYear();
+
+// photo gallery
+const cohortEightProfiles = [
+  {
+    name: "",
+    headshot: "",
+    funnyPhoto: "",
+    bio: "",
+  },
+  {
+    name: "",
+    headshot: "",
+    funnyPhoto: "",
+    bio: "",
+  },
+  {
+    name: "",
+    headshot: "",
+    funnyPhoto: "",
+    bio: "",
+  },
+  {
+    name: "",
+    headshot: "",
+    funnyPhoto: "",
+    bio: "",
+  },
+  {
+    name: "",
+    headshot: "",
+    funnyPhoto: "",
+    bio: "",
+  },
+  {
+    name: "",
+    headshot: "",
+    funnyPhoto: "",
+    bio: "",
+  },
+  {
+    name: "",
+    headshot: "",
+    funnyPhoto: "",
+    bio: "",
+  },
+  {
+    name: "",
+    headshot: "",
+    funnyPhoto: "",
+    bio: "",
+  },
+  {
+    name: "",
+    headshot: "",
+    funnyPhoto: "",
+    bio: "",
+  },
+  {
+    name: "",
+    headshot: "",
+    funnyPhoto: "",
+    bio: "",
+  },
+  {
+    name: "",
+    headshot: "",
+    funnyPhoto: "",
+    bio: "",
+  },
+];
